05d705bafce3caa90dcf143eb4284a7c30040dd2
Emeric Krause <ekrause@k0r3p6.42mulhouse.fr>
2025-05-08 16:15:16 +0200

    ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -5,15 +5,23 @@
 int main()
 {
 	FragTrap frag("frag");
-	
+
+	// --Atributes-- //
+	std::cout << std::endl << "Atributes:" << std::endl;
+
+	std::cout
+		<< "Name: " << frag.getName() << std::endl
+		<< "Hit points: " << frag.getHitPoints() << std::endl
+		<< "Energy points: " << frag.getEnergyPoints() << std::endl
+		<< "Attack damage: " << frag.getAttackDamage() << std::endl;
+
+	// --Atributes-- //
+	std::cout << std::endl << "Tests:" << std::endl;
+
 	frag.highFivesGuys();
-	std::cout << frag.getHitPoints() << std::endl;
 	frag.setHitPoints(20);
 	std::cout << frag.getHitPoints() << std::endl;
 
-	FragTrap frag2(frag);
-	frag2.highFivesGuys();
-	std::cout << frag.getHitPoints() << std::endl;
-
+	std::cout << std::endl;
 	return (0);
 }
